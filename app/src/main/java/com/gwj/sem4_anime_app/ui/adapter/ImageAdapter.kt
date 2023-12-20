4a0cb8ba262afc9f0f92257206860eaf530008c2
TBNR-1257 <tzehernbryan22w@forwardschool.edu.my>
2023-12-20 15:07:12 +0800

    Top Anime & item_layout

---
+++ app/src/main/java/com/gwj/sem4_anime_app/ui/adapter/ImageAdapter.kt
@@ -0,0 +1,11 @@
+//package com.gwj.sem4_anime_app.ui.adapter
+//
+//import androidx.recyclerview.widget.RecyclerView
+//import androidx.viewpager2.widget.ViewPager2
+//
+//class ImageAdapter(
+//    private val imageList: ArrayList<Int>,
+//    private val viewPager2: ViewPager2
+//): RecyclerView.Adapter<ImageAdapter.ImageViewHolder>() {
+//
+//}
