7cae8ea4e75102fb043a47a9727a9ed218cc65f8
gwaijian <gwaijian@gmail.com>
2024-01-02 16:38:39 +0800

    not complete

--- app/src/main/java/com/gwj/sem4_anime_app/ui/vidview/VideoFragment.kt
+++ app/src/main/java/com/gwj/sem4_anime_app/ui/vidview/VideoFragment.kt
@@ -1,18 +1,16 @@
 package com.gwj.sem4_anime_app.ui.vidview
 
 import android.net.Uri
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import com.gwj.recipesapp.ui.base.BaseFragment
-import com.gwj.recipesapp.ui.base.BaseViewModel
-import com.gwj.sem4_anime_app.R
 import com.gwj.sem4_anime_app.databinding.FragmentVideoBinding
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.YouTubePlayerCallback
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -20,8 +18,6 @@ import dagger.hilt.android.AndroidEntryPoint
 class VideoFragment : BaseFragment<FragmentVideoBinding>() {
 
     override val viewModel: VideoViewModel by viewModels()
-
-    //TODO use youtube iframe,android-youtube-player? exo-player is hard
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -38,16 +34,17 @@ class VideoFragment : BaseFragment<FragmentVideoBinding>() {
         lifecycle.addObserver(youTubePlayerView)
 
         val fullUrl =
-            "https://www.youtube.com/watch?v=3M1BTYwUYLs" // replace with the URL from the API
+            "https://www.youtube.com/watch?v=EoxRhxsTmNg" // replace with the URL from the API
         val videoId = Uri.parse(fullUrl).getQueryParameter("v")
 
         youTubePlayerView.addYouTubePlayerListener(object : AbstractYouTubePlayerListener() {
-            override fun onReady(youTubePlayer: com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer) {
+            override fun onReady(youTubePlayer: YouTubePlayer) {
                 if (videoId != null) {
                     youTubePlayer.loadVideo(videoId, 0f)
                 }
             }
         })
+
     }
 
     override fun setupViewModelObserver() {
