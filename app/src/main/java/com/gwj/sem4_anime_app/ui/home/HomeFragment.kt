7f5c1157a513eeccdf54d17fe48becd22fdd655f
Goh Wai Jian <gwaijian@gmail.com>
2024-01-19 11:08:11 +0800

    remove useless item

--- app/src/main/java/com/gwj/sem4_anime_app/ui/home/HomeFragment.kt
+++ app/src/main/java/com/gwj/sem4_anime_app/ui/home/HomeFragment.kt
@@ -1,7 +1,6 @@
 package com.gwj.sem4_anime_app.ui.home
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
